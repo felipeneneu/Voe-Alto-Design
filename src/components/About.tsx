99de6d6741d35d035089d2749352d059025e012a
felipeneneu <feliperpg3@gmail.com>
2024-08-20 21:44:48 -0300

    Pequenos ajustes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,34 +1,25 @@
-import { ArrowClockwise, ChatCircle, Heart } from "@phosphor-icons/react"
 import './Tweet.css'
 import { Slider } from "./Slider"
+import avatarempresa from '../assets/Ativo 2.png'
+import dono from '../assets/Felipe-Avatar.jpg'
 
 export function About() {
   return (
     <div className='tweet'>
-      <img src="https://openseauserdata.com/files/4c7b1857e28506d0097052584355f4f3.png" alt="" />
+      <img src={avatarempresa} alt="" />
 
       <div className='tweet-content'>
         <div className='tweet-content-header'>
           <strong>Felipe Neneu</strong>
           <span>@Cto</span>
         </div>
-
-        <Slider />
-
-        <div className='tweet-content-footer'>
-          <button type='button'>
-            <ChatCircle />
-            20
-          </button>
-          <button type='button'>
-            <ArrowClockwise />
-            20
-          </button>
-          <button type='button'>
-            <Heart />
-            20
-          </button>
+        <div className='nosite'>
+          <img className='dono' src={dono} alt="" />
+          <h2>Quem sou eu?</h2>
+          <p>🎨 Sou designer desde os 12 anos, quando transformei meu hobby em paixão ao criar sites de animes e aprender HTML e CSS. Com o tempo, desenvolvi minhas habilidades em softwares como Photoshop e Illustrator,
+            especializando-me em Identidade Visual, criação de flyers, posts para Instagram 📱 e motion design para reels 🎥.</p>
         </div>
+        <Slider />
       </div>
     </div>
   )
